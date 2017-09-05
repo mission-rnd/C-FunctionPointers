630729c542470c036226dd2774d3e60b829e09b8
Abhijith Ravuri <souravabhijith@gmail.com>
2017-09-05 20:02:39 +0530

    Final commit

--- src/FunctionHeaders.h
+++ src/FunctionHeaders.h
@@ -1,5 +1,4 @@
-int solve_nsnipers(int *, int);
-int *spiral(int rows, int columns, int **input_array);
-int path_exists(int *maze, int rows, int columns, int x1, int y1, int x2, int y2);
-int get_steps(int s);
-int count_pairs_wrapper(char *str, int len);
+void test_functionpointers_starter();
+void test_voidpointers();
+void test_max_subarray();
+void test_combine_common_elements();
